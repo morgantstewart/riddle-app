9d85ffbb4f1502725216156047fe6a60d1da56af
morgantstewart <morgan@paysigil.com>
2025-07-31 16:53:42 -0400

    safe stop point

--- models/riddle.js
+++ models/riddle.js
@@ -1,16 +1,9 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const express = require('express');
-const app = express();
+
 const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-const session = require('express-session');
-
-const port = process.env.PORT ? process.env.PORT : '3000';
 
 
-const RiddlesSchema = mongoose.Schema({
+
+const riddlesSchema = mongoose.Schema({
   riddle: {
     type: String,
     required: true,
@@ -25,3 +18,7 @@ const RiddlesSchema = mongoose.Schema({
   },
 });
 
+
+const Riddle = mongoose.model('Riddle', riddlesSchema);
+module.exports = Riddle;
+//create a model, created variable and named it
